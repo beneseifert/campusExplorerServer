386a490765dccab244eeb17b341084e7b20bff7e
Peter Ehrich <p.ehrich@campus.lmu.de>
2018-12-09 17:42:30 +0100

    Basic webscraping tests of LSF

--- src/main/java/de/lmu/ifi/mobile/msp/RestTest.java
+++ src/main/java/de/lmu/ifi/mobile/msp/RestTest.java
@@ -1,7 +1,6 @@
 package de.lmu.ifi.mobile.msp;
 
-import com.jaunt.JauntException;
-import com.jaunt.UserAgent;
+import com.jaunt.*;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,20 +11,61 @@ public class RestTest {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String getAllMessages() {
-        try{
-            UserAgent userAgent = new UserAgent();                       //create new userAgent (headless browser).
-            userAgent.visit("http://oracle.com");                        //visit a url  
-            return userAgent.doc.innerHTML();               //print the document as HTML
-          }
-          catch(JauntException e){         //if an HTTP/connection error occurs, handle JauntException.
+        try {
+            UserAgent userAgent = new UserAgent(); // create new userAgent (headless browser).
+             userAgent.visit("http://oracle.com");
+            return userAgent.doc.innerHTML(); // print the document as HTML
+        } catch (JauntException e) { // if an HTTP/connection error occurs, handle JauntException.
             System.err.println(e);
-          }
+        }
         return "didn't work";
     }
 
-    // @RequestMapping(value = "/sendMessage", method = RequestMethod.POST, consumes = "application/json")
-    // public ResponseEntity<HttpStatus> receiveMessage(@RequestBody Message message) {
-    //     MessageModel.addMessage(message);
-    //     return ResponseEntity.ok(HttpStatus.OK);
+    @RequestMapping(value = "/1", method = RequestMethod.GET)
+    public String getCancellations() {
+        try {
+        UserAgent userAgent = new UserAgent();
+        userAgent.visit("https://lsf.verwaltung.uni-muenchen.de/qisserver/rds?state=currentLectures&type=1&next=CurrentLectures.vm&nextdir=ressourcenManager&navigationPosition=functions%2CcanceledLectures&breadcrumb=canceledLectures&topitem=locallinks&subitem=canceledLectures&asi=");
+        Elements courses = userAgent.doc.findEvery("<div class=content>").findEvery("<a>");
+
+        Element date = userAgent.doc.findFirst("<h2>");
+        
+        if (courses.size() == 0){
+            return date.innerHTML() + " - Keine";
+        }else{
+            return date.innerHTML() + courses.innerHTML();
+        }
+        
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        return "nope";
+    }
+
+
+    @RequestMapping(value = "/2", method = RequestMethod.GET)
+    public String getLectureDirectory() {
+        try {
+        UserAgent userAgent = new UserAgent();
+        userAgent.visit("https://lsf.verwaltung.uni-muenchen.de/qisserver/rds?state=wtree&search=1&category=veranstaltung.browse&navigationPosition=functions%2Clectureindex&breadcrumb=lectureindex&topitem=locallinks&subitem=lectureindex");
+        
+        Elements links = userAgent.doc.findEvery("<a class=ueb>");
+        
+        return links.innerHTML();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        return "nope";
+    }
+
+
+
+
+    // @RequestMapping(value = "/sendMessage", method = RequestMethod.POST, consumes
+    // = "application/json")
+    // public ResponseEntity<HttpStatus> receiveMessage(@RequestBody Message
+    // message) {
+    // MessageModel.addMessage(message);
+    // return ResponseEntity.ok(HttpStatus.OK);
     // }
 }
