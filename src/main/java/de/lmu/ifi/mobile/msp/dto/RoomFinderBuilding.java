4f53057731267ae19405af30a3aa940d477c9f20
Benedikt Seifert <bse@lt457.iteratec.de>
2018-12-09 15:47:57 +0100

    Import room finder data and add DTOs

---
+++ src/main/java/de/lmu/ifi/mobile/msp/dto/RoomFinderBuilding.java
@@ -0,0 +1,62 @@
+package de.lmu.ifi.mobile.msp.dto;
+
+public class RoomFinderBuilding {
+
+    private String code;
+    private String displayName;
+    private String lat;
+    private String lng;
+    private String city;
+    private String hasImage;
+
+    public RoomFinderBuilding() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getHasImage() {
+        return hasImage;
+    }
+
+    public void setHasImage(String hasImage) {
+        this.hasImage = hasImage;
+    }
+}
---
+++ src/main/java/de/lmu/ifi/mobile/msp/dto/RoomFinderBuildings.java
@@ -0,0 +1,19 @@
+package de.lmu.ifi.mobile.msp.dto;
+
+import java.util.List;
+
+public class RoomFinderBuildings {
+
+    private List<RoomFinderBuilding> buildings;
+
+    public RoomFinderBuildings() {
+    }
+
+    public List<RoomFinderBuilding> getBuildings() {
+        return buildings;
+    }
+
+    public void setBuildings(List<RoomFinderBuilding> buildings) {
+        this.buildings = buildings;
+    }
+}
